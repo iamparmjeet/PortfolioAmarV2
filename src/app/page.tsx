71607892b2131f565b2a742c5bafa657a0e21309
Parmjeet <iamparmjeetmishra@gmail.com>
2025-02-23 11:36:49 +0530

    Bg-dotted added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 export default function LandingPage() {
 	return (
-		<div className="geistMono font-bold text-2xl">LandingPage</div>
+		<div
+			className="[perspective::1000px] [transform-style:preserve-3d] h-screen w-full bg-neutral-900 flex items-center justify-center text-white"
+			style={{
+				backgroundImage: `radial-gradient(circle at 0.5px 0.5px,rgba(6,182,212,0.2), 0.5px, transparent 0)`,
+				backgroundSize: "8px 8px",
+				backgroundRepeat: "repeat",
+			}}
+		>
+			LandingPage
+		</div>
 	);
 }
