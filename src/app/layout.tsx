df2b9a0856287eb4c64eeb8ebb04882eea937f38
Parmjeet <iamparmjeetmishra@gmail.com>
2025-02-23 11:20:57 +0530

    geist font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,37 @@
-import type { Metadata } from "next"
-import { Providers } from "./components/providers"
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Providers } from "./components/providers";
+import "./globals.css";
 
-import "./globals.css"
+const geistSans = localFont({
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
+});
+const geistMono = localFont({
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
+});
 
 export const metadata: Metadata = {
-  title: "JStack App",
-  description: "Created using JStack",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
-}
+	title: "Amar Editz",
+	description: "Video Editz with love",
+	icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="antialiased">
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				<Providers>{children}</Providers>
+			</body>
+		</html>
+	);
 }
