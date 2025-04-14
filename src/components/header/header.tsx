7fc5123df756875534074ec02a1727ab8e8ae1a3
parm <iamparmjeetmishra@gmail.com>
2025-04-14 15:09:53 +0530

    Header done

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { IconMenu, IconX } from "@tabler/icons-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 import Logo from "@/components/header/logo";
@@ -15,65 +16,80 @@ const navigation = [
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const pathname = usePathname();
 
   return (
-
-    <header
-      className="flex items-center justify-between transition-all duration-300"
-    >
-      <Logo />
-      {/* <NavHeader /> */}
-      {/* Desktop Navigation */}
-      <nav className="hidden md:flex items-center space-x-8">
-        {navigation.map(item => (
-          <Link key={item.name} href={item.href} className="text-gray-300 hover:text-amber-400 transition-colors">
-            {item.name}
-          </Link>
-        ))}
-        <Link
-          href="#contact"
-          className="px-4 py-2 bg-gold-500 hover:bg-gold-600 text-black font-medium rounded-md transition-colors"
-        >
-          Let's Talk
-        </Link>
-      </nav>
-      {/* Mobile Menu Button */}
-      <Button
-        type="button"
-        className="md:hidden text-gray-300"
-        onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+    <>
+      <header
+        className="container w-full relative flex items-center justify-between transition-all py-4 px-2 md:my-4 mb-4 rounded-md bg-lime-400/5 duration-300"
       >
-        {
-          mobileMenuOpen
-            ? <IconX className="h-6 w-6" />
-            : <IconMenu className="h-6 w-6" />
-        }
-      </Button>
-
+        <Logo />
+        {/* Desktop Navigation */}
+        <nav className="hidden md:flex items-center space-x-2 text-lime-500 transition-colors">
+          {navigation.map(item => (
+            <Link
+              key={item.name}
+              href={item.href}
+              className={`"hover:bg-lime-600 hover:text-lime-950 px-4 py-2 rounded-md transition-colors" 
+            ${pathname === item.href
+              ? "bg-lime-600 text-lime-950 "
+              : "hover:bg-lime-600"
+            } `}
+            >
+              {item.name}
+            </Link>
+          ))}
+          <LetsTalkButton />
+        </nav>
+        {/* Mobile Menu Button */}
+        <Button
+          type="button"
+          variant="ghost"
+          className="md:hidden bg-lime-600/80 text-lime-400 cursor-pointer"
+          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+        >
+          {
+            mobileMenuOpen
+              ? <IconX className="h-6 w-6" />
+              : <IconMenu className="h-6 w-6" />
+          }
+        </Button>
+      </header>
       {/* Mobile Navigation */}
       {mobileMenuOpen && (
-        <div className="md:hidden bg-black/95 backdrop-blur-md">
-          <div className="container mx-auto px-4 py-4 space-y-4">
-            {navigation.map(item => (
-              <Link
-                key={item.name}
-                href={item.href}
-                className="block text-gray-300 hover:text-amber-400 transition-colors py-2"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                {item.name}
-              </Link>
-            ))}
-            <Link
-              href="#contact"
-              className="block px-4 py-2 bg-gold-500 hover:bg-gold-600 text-black font-medium rounded-md transition-colors text-center"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              Let's Talk
-            </Link>
+        <header className="realtive z-50 w-full h-full">
+          <div className="absolute left-0 right-0 bg-lime-950 rounded-md w-full p-4 md:hidden ">
+            <ul className="flex items-start flex-col space-y-2 mb-6">
+              {navigation.map(item => (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  onClick={() => setMobileMenuOpen(false)}
+                  className={`"hover:bg-lime-600 hover:text-lime-950 w-full px-4 py-1.5 rounded-md transition-colors" 
+                  ${pathname === item.href
+                  ? "bg-lime-600 text-lime-950 "
+                  : "hover:bg-lime-600"
+                } `}
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </ul>
+            <LetsTalkButton />
           </div>
-        </div>
+        </header>
       )}
-    </header>
+    </>
+  );
+}
+
+function LetsTalkButton() {
+  return (
+    <Link
+      href="#contact"
+      className="px-4 py-2 bg-lime-600 text-lime-950 hover:bg-lime-600/80 hover:text-lime-400 font-medium rounded-md transition-colors"
+    >
+      Let's Talk
+    </Link>
   );
 }
