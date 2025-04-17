2ca4404c4728df4ad442415f4b10a730ada6ff92
parm <iamparmjeetmishra@gmail.com>
2025-04-18 02:39:52 +0530

    Data updated with new images

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,6 +2,10 @@ const URL = "https://media.amarjeetmishra.com";
 
 export const LogoImg = `${URL}/images/logo.png`;
 export const placeholderImg = `${URL}/images/placeholder.svg`;
+export const bg1 = `${URL}/images/Bg-gradient1.jpg`;
+export const bg1_min = `${URL}/images/bg-gradient.webp`;
+export const bg2_min = `${URL}/images/bg-gradient1.webp`;
+export const bg2_min_jpg = `${URL}/images/bg-gradient-min.jpg`;
 
 export const HeroData = [
 
