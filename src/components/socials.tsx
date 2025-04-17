094e471e3d5909a793e4e4d42b14d1221766ca0f
parm <iamparmjeetmishra@gmail.com>
2025-04-18 02:39:07 +0530

    Socials updated with new colors and text

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -4,28 +4,31 @@ import Link from "next/link";
 const Socials = [
   {
     id: 1,
+    label: "Follow on Instagram",
     href: "https://www.instagram.com/amarjeetmishra001/",
     icon: (
       <IconBrandInstagram
-        className="hover:fill-lime-400/5 hover:stroke-lime-500 w-7 h-7 "
+        className="hover:fill-neutral-400 hover:stroke-neutral-300 w-7 h-7 "
       />
     ),
   },
   {
     id: 2,
+    label: "Follow on Instagram",
     href: "#",
     icon: (
       <IconBrandTwitter
-        className="hover:fill-lime-400/5 hover:stroke-lime-500 w-7 h-7 "
+        className="hover:fill-neutral-400 hover:stroke-neutral-300 w-7 h-7 "
       />
     ),
   },
   {
     id: 3,
+    label: "Subscribe on Youtube",
     href: "#",
     icon: (
       <IconBrandYoutube
-        className="hover:fill-lime-400/5 hover:stroke-lime-500 w-7 h-7 "
+        className="hover:fill-neutral-400 hover:stroke-neutral-300 w-7 h-7 "
       />
     ),
   },
@@ -33,12 +36,13 @@ const Socials = [
 
 export default function SocialsBox() {
   return (
-    <div className="flex space-x-4">
+    <div className="space-y-4 ">
       {Socials.map(social => (
         <SocialIcon
           key={social.id}
           href={social.href}
           icon={social.icon}
+          label={social.label}
         />
       ))}
     </div>
@@ -47,16 +51,18 @@ export default function SocialsBox() {
 
 type SocialIconType = {
   href: string;
+  label: string;
   icon: React.ReactNode;
 };
 
-function SocialIcon({ href, icon }: SocialIconType) {
+function SocialIcon({ href, icon, label }: SocialIconType) {
   return (
     <Link
       href={href}
-      className="text-lime-600/90 transition-colors"
+      className="flex items-center gap-2 text-neutral-400 hover:text-neutral-300 transition-colors"
     >
       {icon}
+      <p>{ label }</p>
     </Link>
   );
 }
