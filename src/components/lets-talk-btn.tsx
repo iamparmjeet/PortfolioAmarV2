4b00248f5e648dd77dd9080477ac6681dbb9e5ff
parm <iamparmjeetmishra@gmail.com>
2025-04-22 02:29:17 +0530

    Hidden in mobile fixed

--- src/components/lets-talk-btn.tsx
+++ src/components/lets-talk-btn.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/moving-border";
 
 export default function LetsTalkButton() {
   return (
-    <Button className="hidden md:block"  >
+    <Button className="md:block"  >
       <Link
         className="flex items-center justify-center h-full text-base font-medium"
         href="#contact"
