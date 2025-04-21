ef22034c9644695e39a0e0a672cf8347db534699
parm <iamparmjeetmishra@gmail.com>
2025-04-22 00:30:56 +0530

    Sparkles effect componen added

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,33 +1,50 @@
+import Image from "next/image";
 import Link from "next/link";
 
+import { Cover } from "@/components/ui/cover";
+import { HeroImg } from "@/lib/data";
+
 import { Button } from "./ui/button";
+import { ContainerTextFlip } from "./ui/container-text-flip";
 
 export default function HeroSection() {
   return (
-    <>
-      <section className="contianer h-screen flex overflow-hidden p-4 ">
-        <div className="text-center mx-auto px-4 relative z-20">
-          <h1 className="text-5xl md:text-8xl font-extrabold mb-4 bg-gradient-to-r from-lime-400 to-teal-700 bg-clip-text text-transparent">
-            <span>Visual</span>
-            {" "}
-            <span>Storyteller</span>
-          </h1>
-          <h2 className="text-3xl md:text-5xl text-center font-mediumv text-gold-300 mb-6">Video Editor & Content Creator</h2>
-          <p className="text-lg mx-auto md:text-xl text-lime-600 mb-8 max-w-2xl">
-            Transforming ideas into compelling visual narratives for property dealers, beauty professionals, and
-            businesses across industries.
-          </p>
+    <section className="flex bg-black rounded-2xl overflow-hidden mb-4 text-white p-6">
+      <div className="w-1/2">
+        <Image
+          src={HeroImg}
+          alt="Hero Image"
+          width={700}
+          height={700}
+          className="rounded "
+        />
+      </div>
+      <div className="flex flex-col w-1/2 gap-8 mx-auto ">
+        <h1 className="text-3xl md:text-6xl font-extrabold leading-tight tracking-tight ">
+          <span>
+            Unlock The Power of
+          </span>
+          <ContainerTextFlip
+            animationDuration={1000}
+            words={["Video Campaigns", "Insta Campaigns"]}
+          />
+        </h1>
+        <h2 className="text-3xl md:text-5xl font-mediumv text-gold-300 mb-6">Launch your video compaign in hours, not days</h2>
+        <p className="text-lg mx-auto md:text-xl text-lime-600 mb-8 max-w-2xl">
+          Transforming ideas into compelling visual narratives for property dealers, beauty professionals, and
+          businesses across industries.
+        </p>
 
-          <Button asChild>
-            <Link
-              href="#contact"
-              className="px-8 py-6  border text-2xl font-medium rounded-md transition-colors"
-            >
-              Let's Talk
-            </Link>
-          </Button>
-        </div>
-      </section>
-    </>
+        <Button asChild>
+          <Link
+            href="#contact"
+            className="px-8 py-6  border text-2xl font-medium rounded-md transition-colors"
+          >
+            Let's Talk
+          </Link>
+        </Button>
+      </div>
+
+    </section>
   );
 }
