646d9e3a5f6a091389b9a0cb61b27f7bb61b5f52
parm <iamparmjeetmishra@gmail.com>
2025-04-22 01:59:24 +0530

    bg-removed

--- src/app/(pages)/(Home)/page.tsx
+++ src/app/(pages)/(Home)/page.tsx
@@ -8,18 +8,10 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      <div
-        className={`absolute w-full h-[min(250vh,1800px)] top-0 bg-cover bg-top pointer-events-none -z-10 bg-[url("/images/bg1.jpg")] `}
-      >
-      </div>
       <ServicesSection />
       <PortfolioSection />
       <ClientSection />
       <ContactSection />
-      <div
-        className={`absolute w-full h-[min(250vh,1800px)] bottom-0 bg-cover bg-top pointer-events-none -z-10 bg-[url("/images/bg-gradient-min.jpg")] `}
-      >
-      </div>
     </>
   );
 }
