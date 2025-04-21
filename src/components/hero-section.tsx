7ead0f84f653d8377824583b991689fa1347f64c
parm <iamparmjeetmishra@gmail.com>
2025-04-22 01:58:24 +0530

    Hero Section Finish

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,16 +1,12 @@
 import Image from "next/image";
-import Link from "next/link";
-
-import { Cover } from "@/components/ui/cover";
 import { HeroImg } from "@/lib/data";
-
-import { Button } from "./ui/button";
 import { ContainerTextFlip } from "./ui/container-text-flip";
+import LetsTalkButton from "./lets-talk-btn";
 
 export default function HeroSection() {
   return (
-    <section className="flex bg-black rounded-2xl overflow-hidden mb-4 text-white p-6">
-      <div className="w-1/2">
+    <section className="flex flex-col gap-6 md:flex-row bg-black rounded-2xl overflow-hidden my-4 md:my-6 text-white p-6">
+      <div className="md:w-1/2">
         <Image
           src={HeroImg}
           alt="Hero Image"
@@ -19,30 +15,22 @@ export default function HeroSection() {
           className="rounded "
         />
       </div>
-      <div className="flex flex-col w-1/2 gap-8 mx-auto ">
+      <div className="flex flex-col justify-center items-start  md:w-1/2 gap-6 mx-auto ">
         <h1 className="text-3xl md:text-6xl font-extrabold leading-tight tracking-tight ">
           <span>
             Unlock The Power of
           </span>
           <ContainerTextFlip
             animationDuration={1000}
-            words={["Video Campaigns", "Insta Campaigns"]}
+            words={["Video Campaigns", "Insta Campaigns", "Youtube Videos", "Podcasts"]}
           />
         </h1>
-        <h2 className="text-3xl md:text-5xl font-mediumv text-gold-300 mb-6">Launch your video compaign in hours, not days</h2>
-        <p className="text-lg mx-auto md:text-xl text-lime-600 mb-8 max-w-2xl">
+        <h2 className="text-xl md:text-3xl font-medium">Launch your video compaign in hours, not days</h2>
+        <p className="text-base md:text-xl md:text-balance">
           Transforming ideas into compelling visual narratives for property dealers, beauty professionals, and
           businesses across industries.
         </p>
-
-        <Button asChild>
-          <Link
-            href="#contact"
-            className="px-8 py-6  border text-2xl font-medium rounded-md transition-colors"
-          >
-            Let's Talk
-          </Link>
-        </Button>
+        <LetsTalkButton />
       </div>
 
     </section>
