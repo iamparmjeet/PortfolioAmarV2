4b1386e359ccbe07b3b3d9e86463f808b6b34892
parm <iamparmjeetmishra@gmail.com>
2025-04-23 20:51:36 +0530

    Remove Testing div

--- src/app/(pages)/(Home)/page.tsx
+++ src/app/(pages)/(Home)/page.tsx
@@ -1,24 +1,11 @@
-import Image from "next/image";
-
 import ClientSection from "@/components/client-section";
 import HeroSection from "@/components/hero-section";
 import PortfolioSection from "@/components/portfolio-section";
 import ServicesSection from "@/components/services-section";
 
 export default async function Home() {
-  const data = await fetch("https://media.amarjeetmishra.com/amarjeet-portfolio/assets/images/Jewellery/1.webp");
-  console.log("Data", data);
-
   return (
     <>
-      <div className="cotainer p-6">
-        <Image
-          src="https://media.amarjeetmishra.com/amarjeet-portfolio/assets/images/Jewellery/1.webp"
-          alt="Jewellery"
-          fill
-          priority
-        />
-      </div>
       <HeroSection />
       <ServicesSection />
       <PortfolioSection />
