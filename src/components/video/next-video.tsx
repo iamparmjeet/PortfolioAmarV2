50538ec0f8a95aa804d200d8a4487c2be371388e
parm <iamparmjeetmishra@gmail.com>
2025-04-24 02:31:46 +0530

    Player updated

--- src/components/video/next-video.tsx
+++ src/components/video/next-video.tsx
@@ -1,30 +1,31 @@
-import Video from "next-video";
+import Video from "next-video/player";
 import Image from "next/image";
 import Instaplay from "player.style/instaplay/react";
 
 type VideoProps = {
   href: string;
-  thumbnail: string;
+  thumbnail?: string;
 };
 
 export default function NextVideo({ href, thumbnail }: VideoProps) {
   return (
-    <>
-      <Video
-        src={href}
-        theme={Instaplay}
-        style={{
-          width: "400px",
-        }}
-      >
-        <Image
-          slot="poster"
-          src={thumbnail}
-          alt="thumbnail"
-          width={300}
-          height={600}
-        />
-      </Video>
-    </>
+    <Video
+      src={href}
+      className="w-full h-full object-cover"
+      autoPlay={false}
+      muted={false}
+      controls={true}
+      // theme={Instaplay}
+    >
+      <Image
+        src={thumbnail || ""}
+        slot="poster"
+        placeholder="blur"
+        blurDataURL="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+P+/HgAFhAJ/wlseKgAAAABJRU5ErkJggg=="
+        width={720}
+        height={1280}
+        alt="Video thumbnail"
+      />
+    </Video>
   );
 }
