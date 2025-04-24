a8773f1bec66e3ede3e60545968a18a77d694ffb
parm <iamparmjeetmishra@gmail.com>
2025-04-25 03:50:20 +0530

    Fixed Colors

--- src/components/lets-talk-btn.tsx
+++ src/components/lets-talk-btn.tsx
@@ -9,7 +9,7 @@ type ButtonProps = {
 
 export default function LetsTalkButton({ text, href }: ButtonProps) {
   return (
-    <Button borderRadius="1.75rem" className="relative z-10 bg-white text-black hover:bg-gray-100">
+    <Button borderRadius="1.75rem" className="relative z-10 ">
       <Link
         className="flex items-center justify-center h-full text-base font-medium "
         href={href || "/contact"}
--- src/components/ui/moving-border.tsx
+++ src/components/ui/moving-border.tsx
@@ -6,7 +6,7 @@ import {
   useMotionValue,
   useTransform,
 } from "motion/react";
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 
 import { cn } from "@/lib/utils";
 
@@ -32,7 +32,7 @@ export function Button({
   return (
     <Component
       className={cn(
-        "relative h-16 w-40 overflow-hidden bg-transparent p-[1px] text-xl",
+        "relative h-12 w-30 overflow-hidden bg-transparent p-[2px] text-xl",
         containerClassName,
       )}
       style={{
@@ -56,7 +56,7 @@ export function Button({
 
       <div
         className={cn(
-          "relative flex h-full w-full items-center justify-center border border-slate-800 bg-slate-900/[0.8] text-sm text-white antialiased backdrop-blur-xl",
+          "relative flex h-full w-full items-center justify-center border border-orange-600 bg-linear-to-r from-orange-600 to-amber-600 hover:bg-linear-to-l text-sm text-white antialiased backdrop-blur-xl",
           className,
         )}
         style={{
