8406ef464a48f5557abea9a79b2690fd2a43c120
parm <iamparmjeetmishra@gmail.com>
2025-04-25 21:56:27 +0530

    Heading component

---
+++ src/components/heading.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+
+type OrangeHeading1Type = {
+  text: string;
+  className?: string;
+};
+
+export function OrangeHeading1({ text = "Amar Editz", className }: OrangeHeading1Type) {
+  return (
+    <h2 className={cn("bg-gradient-to-r from-orange-500 to-yellow-500 bg-clip-text text-transparent text-3xl md:text-4xl font-bold", className)}>
+      {text}
+    </h2>
+  );
+}
