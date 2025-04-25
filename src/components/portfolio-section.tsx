f3807a8296c63133b7b2a49521f6d46ab05a02f1
parm <iamparmjeetmishra@gmail.com>
2025-04-25 21:55:38 +0530

    Heading component replaced

--- src/components/portfolio-section.tsx
+++ src/components/portfolio-section.tsx
@@ -1,3 +1,4 @@
+import { OrangeHeading1 } from "./heading";
 import LetsTalkButton from "./lets-talk-btn";
 import PortfolioSectionWithFilter from "./portfolios-all";
 
@@ -5,9 +6,7 @@ export default function PortfolioSection() {
   return (
     <section id="portfolio" className="p-6 md:py-12 bg-black my-6 rounded-2xl text-white flex flex-col items-center gap-10">
       <div className="text-center">
-        <h2 className="bg-gradient-to-r from-orange-500 to-yellow-500 bg-clip-text text-transparent text-3xl md:text-5xl font-extrabold">
-          My Portfolio
-        </h2>
+        <OrangeHeading1 text="My Portfolio Works" className="text-3xl md:text-5xl font-extrabold" />
         <p className="text-neutral-300 text-base md:text-xl max-w-xl mx-auto mt-6">
           Explore my work across different industries and project types
         </p>
