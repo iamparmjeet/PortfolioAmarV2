119ce163074e176b418131bac6fe9bfb665f6c24
parm <iamparmjeetmishra@gmail.com>
2025-04-25 21:54:06 +0530

    Orange Heading

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,7 +1,9 @@
+import { OrangeHeading1 } from "@/components/heading";
+
 export default function AboutPage() {
   return (
     <div className="p-6 bg-black text-white rounded-2xl mt-6">
-      AboutPage
+      <OrangeHeading1 text="About me" />
       <p className="mt-2">Data is coming ....</p>
     </div>
   );
