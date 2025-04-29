d1bfeffbc1174e3907844bf4eed0e9503eb1e5e8
parm <iamparmjeetmishra@gmail.com>
2025-04-29 22:26:07 +0530

    size fixed

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -7,7 +7,7 @@ type OrangeHeading1Type = {
 
 export function OrangeHeading1({ text = "Amar Editz", className }: OrangeHeading1Type) {
   return (
-    <h2 className={cn("bg-gradient-to-r from-orange-500 to-yellow-500 bg-clip-text text-transparent text-3xl md:text-4xl font-bold", className)}>
+    <h2 className={cn("bg-gradient-to-r from-orange-500 to-yellow-500 bg-clip-text text-transparent text-3xl md:text-5xl font-bold", className)}>
       {text}
     </h2>
   );
