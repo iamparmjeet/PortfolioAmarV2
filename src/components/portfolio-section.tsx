6094ac6266be6bc512a67ac498e906d8077ff432
parm <iamparmjeetmishra@gmail.com>
2025-04-29 23:28:14 +0530

    width issue fix with item-center

--- src/components/portfolio-section.tsx
+++ src/components/portfolio-section.tsx
@@ -5,7 +5,7 @@ import PortfolioSectionWithFilter from "./portfolios-all";
 
 export default function PortfolioSection() {
   return (
-    <Container className="flex-col">
+    <Container className="flex-col items-center">
       <div className="text-center">
         <OrangeHeading1 text="My Portfolio Works" className="text-3xl md:text-5xl font-extrabold" />
         <p className="text-neutral-300 text-base md:text-xl max-w-xl mx-auto mt-6">
